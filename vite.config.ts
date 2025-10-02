b628c5d8f1992572bed92b4958a57bad678c5d3e
zixin-jiang <99054689+zixin-jiang@users.noreply.github.com>
2025-10-01 22:36:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/academic-portfolio/" : "/",
+  base: mode === "production" ? "/zixin-jiang.github.io/" : "/",
   server: {
     host: "::",
     port: 8080,
